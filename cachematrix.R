d12217e395b386e28f5b07cebf9884916833c555
Dmoffatcpt <dominic.moffat@coxpowertrain.com>
2019-11-15 14:28:42 +0000

    Comments added to code.

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,7 @@
-## Correct code functionality without comments
+## Correct code functionality with comments
 
-## Write a short comment describing this function
+## First function constructs a list of 4 functions which are used to generate the 
+## inverse of a supplied matrix using the solve() function.
 
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
@@ -16,15 +17,19 @@ makeCacheMatrix <- function(x = matrix()) {
        getinverse = getinverse)
 }
 
-## Write a short comment describing this function
+## Second function evaluates if there is an existing cached value for the inverse
+## of a matrix. If so, this is displayed. If not, the inverse operation is carried out on
+## the supplied matrix in the function call (x).
 
 cacheSolve <- function(x, ...) {
   ## Return a matrix that is the inverse of 'x'
   m <- x$getinverse()
+  ## If the variable 'm' is not empty, the cached value is returned.
   if(!is.null(m)){
     message("Previous result exists. Retrieving.")
     return(m)
   }
+  ## If 'm' is empty, then the inverse is calculated.
   matrixdata <- x$get()
   m <- solve(matrixdata, ...)
   x$setinverse(m)
