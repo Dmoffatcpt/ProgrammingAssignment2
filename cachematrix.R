f476b71587d771c74bc488cea0f703b7c3fe9c32
Dmoffatcpt <dominic.moffat@coxpowertrain.com>
2019-11-15 14:20:27 +0000

    First commit for code functionality, no comments added.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Correct code functionality without comments
 
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
-
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)){
+    message("Previous result exists. Retrieving.")
+    return(m)
+  }
+  matrixdata <- x$get()
+  m <- solve(matrixdata, ...)
+  x$setinverse(m)
+  m
 }
